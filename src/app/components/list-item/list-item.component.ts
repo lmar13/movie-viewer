7d6199b5c3a5acc914da1ecb888a00a900ed8efc
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-19 12:13:00 +0200

    feat: add first folder and file structure with sample components and services

---
+++ src/app/components/list-item/list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-item',
+  imports: [],
+  templateUrl: './list-item.component.html',
+  styleUrl: './list-item.component.scss'
+})
+export class ListItemComponent {
+
+}

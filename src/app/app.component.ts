1b727df94fe3ee28b824d29e2533c2849d5e6df3
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-19 20:38:03 +0200

    chore: update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import { AsyncPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
-import { ListApiService } from './services/list-api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgFor, AsyncPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'movie-viewer-viabill';
-  movies$: Observable<any>;
-
-  constructor(private listApiService: ListApiService) {
-    this.movies$ = this.listApiService.getMovies();
-  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
@@ -9,7 +9,7 @@ import { httpInterceptor } from './core/auth/http.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideStore(),
     provideHttpClient(withInterceptors([httpInterceptor])),
   ],

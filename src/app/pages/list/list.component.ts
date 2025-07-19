d39745a5c8bbcb3d61cc7f21560ea0d390dd5a7e
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-19 20:38:52 +0200

    feat: move logic connected with movie list to correct page file

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Movie } from '../../models/movie.model';
+import { ListApiService } from '../../services/list-api.service';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [NgFor, AsyncPipe, RouterOutlet, RouterLink],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
+  movies$: Observable<Movie[]>;
+  listApi = inject(ListApiService);
 
+  constructor() {
+    this.movies$ = this.listApi.getMovies();
+  }
 }
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DetailsComponent } from '../components/details/details.component';
+import { ListComponent } from '../pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: ListComponent, children: [{ path: ':movieId', component: DetailsComponent }] },
+];

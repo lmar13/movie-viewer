334c943368406db39957c8114fdb28e3b1ac2d89
Łukasz Marciniak <lmarciniak@clickmeeting.com>
2025-07-19 23:38:17 +0200

    chore: register store in app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
-import { routes } from './routes/app.routes';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { httpInterceptor } from './core/auth/http.interceptor';
+import { routes } from './routes/app.routes';
+import { MoviesEffects } from './store/movies.effects';
+import { moviesReducer } from './store/movies.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideStore(),
+    provideStore({ movies: moviesReducer }),
+    provideEffects([MoviesEffects]),
     provideHttpClient(withInterceptors([httpInterceptor])),
+    ...(isDevMode() ? [provideStoreDevtools()] : []),
   ],
 };
